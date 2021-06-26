7ee79369c539e13e4c9f7713fb13ee8191dca8c1
Jia Guoqing <jiaguoqing12138@gmail.com>
2021-06-26 16:11:35 +0800

    Initial commit

---
+++ tokenizer_lite.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/26 16:01
+# @Author  : gqjia 
+# @Email   : gqjia@iflytek.com
+# @File    : tokenizer_lite.py
+"""Tokenization classes."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import unicodedata
+import os
+
+
+class BasicTokenizer(object):
+    """Runs basic tokenization (punctuation splitting, lower casing, etc.)."""
+
+    def __init__(self):
+        pass
+
+
+
